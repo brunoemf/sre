0f9bb63074b2bf401b6ac5c82564a5c0b2c6c04a
Bruno Figueirôa <bruno@bfigueiroa.com>
2018-06-11 13:44:19 -0300

    changing workload script

--- workload.sh
+++ workload.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 a=0
+site=http://34.228.16.67
 
 while [ $a -lt 100000 ]
 do
-   curl -X GET http://34.228.16.67 > /dev/null
+   curl -X GET $site > /dev/null
    a=`expr $a + 1`
 done
